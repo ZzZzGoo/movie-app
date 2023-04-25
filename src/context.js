00a9c214b26a08662a8fbb2c3e17e28b9831a21c
Vazgen Vardanian <vazgen.eth@gmail.com>
2023-04-25 20:27:44 +0100

    Update context.js

--- src/context.js
+++ src/context.js
@@ -2,7 +2,7 @@
 import React, { useState, useContext, useEffect } from 'react'
 // make sure to use https
 import useFetch from './useFetch'
-export const API_ENDPOINT = `http://www.omdbapi.com/?apikey=dc0d8404`
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=dc0d8404`
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
