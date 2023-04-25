3e4de4724f051d7bc63708f4b907bc1b00f358d3
Vazghen Vardanian <vazgen.eth@gmail.com>
2023-04-25 20:16:48 +0100

    first commit

---
+++ src/context.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useContext, useEffect } from 'react'
+// make sure to use https
+import useFetch from './useFetch'
+export const API_ENDPOINT = `http://www.omdbapi.com/?apikey=61cd5021&`
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState('batman')
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`)
+
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
